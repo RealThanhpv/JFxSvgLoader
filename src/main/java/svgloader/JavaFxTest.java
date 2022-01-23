db9738787e9ab173e8592773736b8902dbc1bc41
RealThanhpv <fireinwinterfell@gmail.com>
2022-01-23 20:44:52 +0700

    remove unused javafx modules

--- src/main/java/svgloader/JavaFxTest.java
+++ src/main/java/svgloader/JavaFxTest.java
@@ -6,69 +6,67 @@
 package svgloader;
 
 /**
- *
  * @author Thanh
  */
 
-	
-import javafx.application.Application;
 
-import javafx.scene.paint.Color;
-import javafx.stage.Stage;
+import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
 
 public class JavaFxTest extends Application {
-	SVGLoader loader = null;
-	public void start(Stage primaryStage) {
-		try {
-			
-			Application.Parameters params = getParameters();
-                        java.util.List<String> pl = params.getRaw();
-                        
-				
-			StackPane  pane = new StackPane();
-						
-			Parent root = new Pane(pane);												
-			Scene scene = new Scene(root);
-			scene.setFill(Color.GRAY);
-                        
-			primaryStage.setScene(scene);			
-			primaryStage.setTitle("Test SVGLoader Window");		
-			
-                        
-                        if(pl.isEmpty())
-                            loader = new SVGLoader("./tiger3.svg");
-                        else
-                            loader = new SVGLoader(pl.get(0));	
-                        long start = System.currentTimeMillis();
-                        Pane svgPane = loader.loadSVG();
-                        svgPane.setScaleX(1);
-                        svgPane.setScaleY(1);
 
 
-                        pane.getChildren().addAll(svgPane);
-                        svgPane.setCache(true);
-                        long end = System.currentTimeMillis();
-                        primaryStage.show();
+    public static void main(String[] args) {
 
-			System.out.println("Total time: "+(end-start)+ " mili secs");
-                  	
-                       
-                      
-                        
-		} catch(Exception e) {		
-		}
-		
-	}
+        launch(args);
+
+    }
+
+    public void start(Stage primaryStage) {
+        try {
+
+            Application.Parameters params = getParameters();
+            java.util.List<String> pl = params.getRaw();
 
 
-	public static void main(String[] args) {
-		
-		launch(args);
-		
-	}
+            StackPane pane = new StackPane();
+
+            Parent root = new Pane(pane);
+            Scene scene = new Scene(root);
+            scene.setFill(Color.GRAY);
+
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Test SVGLoader Window");
+			SVGLoader loader = null;
+
+            if (pl.isEmpty()) {
+				loader = new SVGLoader("./tiger3.svg");
+			}
+            else {
+				loader = new SVGLoader(pl.get(0));
+			}
+            long start = System.currentTimeMillis();
+            Pane svgPane = loader.loadSVG();
+            svgPane.setScaleX(1);
+            svgPane.setScaleY(1);
+
+
+            pane.getChildren().addAll(svgPane);
+            svgPane.setCache(true);
+            long end = System.currentTimeMillis();
+            primaryStage.show();
+
+            System.out.println("Total time: " + (end - start) + " mili secs");
+
+
+        } catch (Exception e) {
+        }
+
+    }
 }
